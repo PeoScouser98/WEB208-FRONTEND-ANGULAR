937ba4a7d1fc6ee2e56fff5ced2433b55c2624cd
Quang Hiệp <quanghiep03198@gmail.com>
2023-02-23 22:55:49 +0700

    using production api

--- src/app/environment/environment.production.ts
+++ src/app/environment/environment.production.ts
@@ -1,6 +1,5 @@
 const env = {
-	baseUrl: 'http://localhost:3000/v1/api',
-	// baseUrl: 'https://web-208-backend.vercel.app/v1/api',
+	baseUrl: "https://web-208-backend.vercel.app/v1/api",
 };
 
 export default env;
